8a7af36082a09c53bd0647623a004a03ecb47084
Steven Feltner <steven.feltner@netapp.com>
2021-07-05 20:27:37 -0700

    Update function call to set credentials

--- main.tf
+++ main.tf
@@ -191,7 +191,7 @@ resource "null_resource" "account_association" {
   depends_on = [azurerm_role_assignment.spot]
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
-    command = "${local.cmd} set-cloud-credentials --account_id ${local.account_id} --client_id ${azuread_application.spot.application_id} --client_secret ${random_string.value.result} --tenant_id ${data.azurerm_client_config.current.tenant_id} --subscription_id ${var.subscription_id}"
+    command = "${local.cmd} set-cloud-credentials --account-id ${local.account_id} --client-id ${azuread_application.spot.application_id} --client-secret ${random_string.value.result} --tenant-id ${data.azurerm_client_config.current.tenant_id} --subscription-id ${var.subscription_id}"
   }
 }
 
